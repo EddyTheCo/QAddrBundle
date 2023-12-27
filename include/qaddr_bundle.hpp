778dbd1380b91354b0cc277e029b4b477d97efbf
Eduardo Gonzalez Lazo <30321688+EddyTheCo@users.noreply.github.com>
2023-12-27 13:23:57 +0100

    Develop (#33)
    * The address box monitors its related outputs.
    
    * It now use nodeConection and account
    
    * Emit all the inputs and address together when rm inputs
    
    * Delete object after clean
    
    * Create a qml module exposing the addressbundle and a qml64 type (#27)
    
    * The address bundle store now the bech32 string and the hrp (#28)
    
    * Set the parent of the addresses
    
    * Set const the creation of unlocks
    
    * Added the address checker qml type (#30)
    
    * Monitor outputs with the address in expire unlock (#31)

--- include/qaddr_bundle.hpp
+++ include/qaddr_bundle.hpp
@@ -1,48 +1,134 @@
 #pragma once
-#include"encoding/qbech32.hpp"
-#include"client/qclient.hpp"
-#include<QString>
-#include<QByteArray>
-#include<set>
+
+#include "block/qinputs.hpp"
+#include "block/qoutputs.hpp"
+#include "block/qunlocks.hpp"
+#include "client/qnode_outputs.hpp"
+
+#if defined(USE_QML)
+#include<QtQml>
+#endif
+
+#include <QObject>
+#include <QtCore/QtGlobal>
+#if defined(WINDOWS_QADDR)
+# define QADDR_EXPORT Q_DECL_EXPORT
+#else
+#define QADDR_EXPORT Q_DECL_IMPORT
+#endif
+
 namespace qiota{
 
 using namespace qblocks;
 
-class AddressBundle
+#if defined(USE_QML)
+class QADDR_EXPORT Qml64:public QObject
 {
+    quint64 m_value;
+    QJsonObject m_json;
+    Q_OBJECT
+
+    Q_PROPERTY(QJsonObject  json READ json  NOTIFY jsonChanged)
+    QML_ELEMENT
+    QML_UNCREATABLE("")
+
 public:
-    AddressBundle(const std::pair<QByteArray,QByteArray>& key_pair_m);
+    Qml64(const quint64 value=0,QObject *parent = nullptr);
+    QJsonObject json(void)const{return m_json;};
+    void setValue(const quint64 value);
+    const auto& getValue()const{return m_value;};
+signals:
+    void jsonChanged();
+private:
+    void intToJSON();
+};
 
-    AddressBundle(const std::shared_ptr<const Address>& addr_m);
+class QADDR_EXPORT AddressChecker:public QObject
+{
+    bool  m_valid;
+    QString m_address;
+    void setValid(bool valid){if(valid!=m_valid){m_valid=valid; emit validChanged();}}
+    Q_OBJECT
 
-    QString get_address_bech32(QString hrp)const;
-    std::shared_ptr<const Address> get_address(void)const;
+    Q_PROPERTY(bool valid READ isValid  NOTIFY validChanged)
+    Q_PROPERTY(QString address MEMBER m_address   NOTIFY addressChanged)
+    QML_ELEMENT
 
+public:
+    AddressChecker(QObject *parent = nullptr);
+    bool isValid(void)const{return m_valid;};
+signals:
+    void validChanged();
+    void addressChanged();
+};
+#endif
+struct InBox {
+    std::shared_ptr<const Input> input=nullptr;
+    std::shared_ptr<const Output> retOutput=nullptr;
+    qblocks::c_array inputHash;
+    std::shared_ptr<Output> output=nullptr;
+    quint64 amount=0;
+};
+class QADDR_EXPORT AddressBox:public QObject
+{
+    Q_OBJECT
+#if defined(USE_QML)
+    Q_PROPERTY(Qml64*  amount READ amountJson CONSTANT)
+    Q_PROPERTY(QString bech32Address READ getAddressBech32 CONSTANT)
+    QML_ELEMENT
+    QML_UNCREATABLE("")
+#endif
 
-    void consume_outputs(std::vector<Node_output> &outs_, const quint64 amount_need_it=0, const quint16 howMany=0);
+public:
+    AddressBox(const std::pair<QByteArray,QByteArray>& keyPair,const QString hrp="rms",QObject *parent = nullptr);
 
+    AddressBox(const std::shared_ptr<const Address>& addr,c_array outId=c_array(),const QString hrp="rms",QObject *parent = nullptr);
 
-    void create_unlocks(const QByteArray & message,const quint16& ref=0);
-    pvector<const Native_Token> get_tokens(const c_array& tokenid="" )const;
+    std::shared_ptr<const Address> getAddress(void)const;
+    QString getAddressHash(void)const;
+    QString getAddressBech32()const{ return m_bech32adddress; }
 
-    void add_tokens(const std::map<qblocks::c_array,quint256>&);
-    qblocks::c_array Inputs_hash;
-    quint64 amount;
-    pvector<const Output> ret_outputs;
-    pvector<Output> alias_outputs,foundry_outputs, nft_outputs;
+    void getOutputs(std::vector<Node_output> &outs, const quint64 amountNeedIt=0, const quint16 howMany=0);
+    pvector<const Unlock> getUnlocks
+        (const QByteArray & message, const quint16 &ref, const size_t &inputSize)const;
+    quint64 amount(void)const{return m_amount;};
+#if defined(USE_QML)
+    Qml64* amountJson()const{return m_amountJson;}
+#endif
+    const auto& inputs(void)const{return m_inputs;};
+    c_array outId()const{return m_outId;}
 
-    pvector<const Input> inputs;
-    std::vector<Output::types> ref_typs;
-    pvector<const Unlock> unlocks;
-    std::map<qblocks::c_array,quint256> native_tokens;
-    std::vector<quint32> to_unlock,to_expire;
-    std::set<QString> otids;
+signals:
+    void amountChanged(quint64 prevA,quint64 nextA);
+    void changed();
+    void inputRemoved(std::vector<c_array>);
+    void inputAdded(c_array);
+    void addrAdded(AddressBox * );
+    void addrRemoved(std::vector<c_array>);
 
 private:
-    const std::pair<QByteArray,QByteArray> key_pair;
-    std::shared_ptr<const Address> addr;
+    void clean(std::vector<c_array> &rm_inputs, std::vector<c_array> &rm_addresses);
+    void monitorToSpend(const c_array outId);
+    void monitorToExpire(const c_array outId, const quint32 unixTime);
+    void monitorFromExpire(const c_array outId, const quint32 unixTime);
+    void monitorToUnlock(const c_array outId,const quint32 unixTime);
+    void rmInput(const c_array outId,std::vector<c_array> & rm_inputs,std::vector<c_array> & rm_addresses);
+    void addInput(const c_array outId, const InBox &inBox);
+    void addAddrBox(const c_array outId, AddressBox* addrBox);
 
+    void setAmount(const quint64 amount){
+        if(amount!=m_amount){const auto oldAmount=m_amount;m_amount=amount;emit amountChanged(oldAmount,amount);}}
+
+    QHash<c_array,InBox> m_inputs;
+    QHash<c_array,AddressBox *> m_AddrBoxes;
+    quint64 m_amount;
+#if defined(USE_QML)
+    Qml64* m_amountJson;
+#endif
+    const std::pair<QByteArray,QByteArray> m_keyPair;
+    const std::shared_ptr<const Address> m_addr;
+    const c_array m_outId;
+    const QString m_bech32adddress,m_hrp;
 
 };
-using address_bundle = AddressBundle;
-}
+};
--- qaddr_bundle.cpp
+++ qaddr_bundle.cpp
@@ -1,64 +1,182 @@
-#include<QJsonDocument>
+#include<QTimer>
 #include"qaddr_bundle.hpp"
-#include <QCryptographicHash>
+#include"encoding/qbech32.hpp"
 #include"crypto/qed25519.hpp"
-#include<QDataStream>
-#include<QDateTime>
-#include<map>
-using namespace qcrypto;
+#include"nodeConnection.hpp"
+
 namespace qiota{
 
 using namespace qblocks;
-
-AddressBundle::AddressBundle(const std::pair<QByteArray,QByteArray>& key_pair_m):key_pair(key_pair_m),
-    addr(std::shared_ptr<Address>(new Ed25519_Address(QCryptographicHash::hash(key_pair.first,QCryptographicHash::Blake2b_256)))),
-    amount(0)
-{ };
-AddressBundle::AddressBundle(const std::shared_ptr<const Address>& addr_m):addr(addr_m),amount(0)
-{ };
-
-
-std::shared_ptr<const Address> AddressBundle::get_address(void)const
+#if defined(USE_QML)
+AddressChecker::AddressChecker(QObject *parent):QObject(parent),m_valid(false)
 {
-    return addr;
-}
-QString AddressBundle::get_address_bech32(QString hrp)const
-{
-    const auto addr=qencoding::qbech32::Iota::encode(hrp,get_address()->addr());
-    return addr;
-}
-void AddressBundle::add_tokens(const std::map<qblocks::c_array,quint256>& others)
-{
-    for (const auto& v : others)
-    {
-        native_tokens[v.first]+=v.second;
-    }
-}
-pvector<const Native_Token> AddressBundle::get_tokens(const c_array &tokenid )const
-{
-    pvector<const Native_Token> var;
-    if(tokenid!="")
-    {
-        auto search = native_tokens.find(tokenid);
-        if(search != native_tokens.end())
+    connect(this, &AddressChecker::addressChanged,this,[=](){
+        const auto addr_pair=qencoding::qbech32::Iota::decode(m_address);
+        if(addr_pair.second.size())
         {
-            var.push_back(Native_Token::Native(search->first,search->second));
+            setValid(true);
         }
-    }
-    else
-    {
-        for (const auto& v : native_tokens)
+        else
         {
-            var.push_back(Native_Token::Native(v.first,v.second));
+            setValid(false);
         }
-    }
-    return var;
-}
-void AddressBundle::create_unlocks(const QByteArray & message, const quint16 &ref)
+    });
+};
+#endif
+AddressBox::AddressBox(const std::pair<QByteArray,QByteArray>& keyPair,
+                       const QString hrp, QObject *parent):QObject(parent),m_keyPair(keyPair),
+    m_addr(std::shared_ptr<Address>(new Ed25519_Address(
+        QCryptographicHash::hash(keyPair.first,QCryptographicHash::Blake2b_256)))),
+    m_amount(0),m_bech32adddress(qencoding::qbech32::Iota::encode(hrp,m_addr->addr())),m_hrp(hrp)
+#if defined(USE_QML)
+    ,m_amountJson(new Qml64(m_amount,this))
+#endif
 {
-    for(const auto& v:inputs)
+#if defined(USE_QML)
+    connect(this, &AddressBox::amountChanged, m_amountJson,[=](quint64 prevA,quint64 nextA)
+            {m_amountJson->setValue(nextA);});
+#endif
+
+};
+AddressBox::AddressBox(const std::shared_ptr<const Address>& addr, c_array outId,const QString hrp,QObject *parent):QObject(parent),
+    m_addr(addr),m_amount(0),m_outId(outId),m_bech32adddress(qencoding::qbech32::Iota::encode(hrp,m_addr->addr()))
+,m_hrp(hrp)
+#if defined(USE_QML)
+    ,m_amountJson(new Qml64(m_amount,this))
+#endif
+{
+#if defined(USE_QML)
+    connect(this, &AddressBox::amountChanged, m_amountJson,[=](quint64 prevA,quint64 nextA)
+            {m_amountJson->setValue(nextA);});
+#endif
+};
+std::shared_ptr<const Address> AddressBox::getAddress(void)const
+{
+    return m_addr;
+}
+QString AddressBox::getAddressHash(void)const
+{
+    return m_addr->addrhash().toHexString();
+}
+void AddressBox::monitorToExpire(const c_array outId,const quint32 unixTime)
+{
+    const auto triger=(unixTime-QDateTime::currentDateTime().toSecsSinceEpoch())*1000;
+    QTimer::singleShot(triger,this,[=](){
+        std::vector<c_array>  rm_inputs;
+        std::vector<c_array>  rm_address;
+        rmInput(outId,rm_inputs,rm_address);
+    });
+}
+void AddressBox::monitorFromExpire(const c_array outId,const quint32 unixTime)
+{
+    const auto triger=(unixTime-QDateTime::currentDateTime().toSecsSinceEpoch())*1000;
+    QTimer::singleShot(triger,this,[=](){
+
+        auto resp=NodeConnection::instance()->rest()->get_api_core_v2_outputs_outputId(outId.toHexString());
+        connect(resp,&Response::returned,this,[=](QJsonValue data){
+            auto node_outputs=std::vector<Node_output>{Node_output(data)};
+            getOutputs({node_outputs});
+            resp->deleteLater();
+        });
+    });
+}
+void AddressBox::monitorToUnlock(const c_array outId, const quint32 unixTime)
+{
+    const auto triger=(unixTime-QDateTime::currentDateTime().toSecsSinceEpoch())*1000;
+    QTimer::singleShot(triger,this,[=](){
+        auto resp=NodeConnection::instance()->rest()->get_api_core_v2_outputs_outputId(outId.toHexString());
+        connect(resp,&Response::returned,this,[=](QJsonValue data){
+            auto node_outputs=std::vector<Node_output>{Node_output(data)};
+            getOutputs({node_outputs});
+            resp->deleteLater();
+        });
+    });
+}
+void AddressBox::monitorToSpend(const c_array outId)
+{
+
+    auto resp=NodeConnection::instance()->mqtt()->get_outputs_outputId(outId.toHexString());
+    connect(resp,&ResponseMqtt::returned,this,[=](QJsonValue data){
+        const auto node_output=Node_output(data);
+        if(node_output.metadata().is_spent_)
+        {
+            resp->deleteLater();
+            std::vector<c_array>  rm_inputs;
+            std::vector<c_array>  rm_address;
+            rmInput(outId,rm_inputs,rm_address);
+        }
+
+    });
+}
+void AddressBox::addInput(const c_array outId, const InBox & inBox)
+{
+    m_inputs.insert(outId,inBox);
+    emit inputAdded(outId);
+
+    if(inBox.output->type()==Output::NFT_typ||inBox.output->type()==Output::Alias_typ)
     {
-        if(addr->type()==Address::Ed25519_typ)
+        AddressBox* nextAddr=nullptr;
+        if(inBox.output->type()==Output::NFT_typ)
+        {
+            nextAddr=new AddressBox(Address::NFT(inBox.output->get_id()),outId,m_hrp,this);
+        }
+        if(inBox.output->type()==Output::Alias_typ)
+        {
+            nextAddr=new AddressBox(Address::Alias(inBox.output->get_id()),outId,m_hrp,this);
+        }
+        addAddrBox(outId,nextAddr);
+    }
+    setAmount(m_amount+inBox.amount);
+}
+void AddressBox::rmInput(const c_array outId, std::vector<c_array> & rm_inputs,std::vector<c_array> & rm_addresses)
+{
+    if(m_inputs.contains(outId))
+    {
+        const auto input=m_inputs.take(outId);
+        const bool isroot=(rm_inputs.size()==0&&rm_addresses.size()==0);
+        quint64 amount=input.amount;
+        rm_inputs.push_back(outId);
+        if(input.output->type()==Output::NFT_typ||input.output->type()==Output::Alias_typ)
+        {
+            const auto addressBox=m_AddrBoxes.take(outId);
+            amount+=addressBox->amount();
+            const auto address=addressBox->getAddress()->addr();
+            rm_addresses.push_back(address);
+            addressBox->clean(rm_inputs,rm_addresses);
+        }
+
+        if(isroot)
+        {
+            emit inputRemoved(rm_inputs);
+            emit addrRemoved(rm_addresses);
+            setAmount(m_amount-amount);
+        }
+
+    }
+
+}
+void AddressBox::addAddrBox(const c_array outId,AddressBox* addrBox)
+{
+    connect(addrBox,&AddressBox::amountChanged,this,[=](const auto prevA,const auto nextA){
+        setAmount(m_amount-prevA+nextA);
+    });
+    m_AddrBoxes.insert(outId,addrBox);
+    emit addrAdded(addrBox);
+}
+void  AddressBox::clean(std::vector<c_array> & rm_inputs,std::vector<c_array> & rm_addresses)
+{
+    for(const auto &v:m_inputs.keys())
+    {
+        rmInput(v,rm_inputs,rm_addresses);
+    }
+    delete this;
+}
+pvector<const Unlock> AddressBox::getUnlocks(const QByteArray & message, const quint16 &ref, const size_t& inputSize) const
+{
+    pvector<const Unlock> unlocks;
+    for(size_t i=0;i<inputSize;i++)
+    {
+        if(m_addr->type()==Address::Ed25519_typ)
         {
             if(unlocks.size())
             {
@@ -66,41 +184,41 @@ void AddressBundle::create_unlocks(const QByteArray & message, const quint16 &re
             }
             else
             {
-                unlocks.push_back(Unlock::Signature(Signature::Ed25519(key_pair.first,qed25519::sign(key_pair,message))));
+                unlocks.push_back(Unlock::Signature(Signature::Ed25519(m_keyPair.first,qcrypto::qed25519::sign(m_keyPair,message))));
             }
         }
-        if(addr->type()==qblocks::Address::Alias_typ)
+        if(m_addr->type()==qblocks::Address::Alias_typ)
         {
-            unlocks.push_back(std::shared_ptr<qblocks::Unlock>(new qblocks::Alias_Unlock(ref)));
+            unlocks.push_back(Unlock::Alias(ref));
         }
-        if(addr->type()==qblocks::Address::NFT_typ)
+        if(m_addr->type()==qblocks::Address::NFT_typ)
         {
-            unlocks.push_back(std::shared_ptr<qblocks::Unlock>(new qblocks::NFT_Unlock(ref)));
+            unlocks.push_back(Unlock::NFT(ref));
         }
     }
-
+    return unlocks;
 }
-void AddressBundle::consume_outputs(std::vector<Node_output> &outs_, const quint64 amount_need_it, quint16 howMany)
+void AddressBox::getOutputs(std::vector<Node_output> &outs_, const quint64 amount_need_it, quint16 howMany)
 {
 
     const auto size=outs_.size();
-    while(((amount_need_it)?amount<amount_need_it:true)&&((howMany>=size)||(!howMany)?!outs_.empty():outs_.size()+howMany>size))
+    while(((amount_need_it)?m_amount<amount_need_it:true)&&((howMany>=size)||(!howMany)?!outs_.empty():outs_.size()+howMany>size))
     {
         const auto v=outs_.back();
-        if(!v.metadata().is_spent_&&otids.insert(v.metadata().outputid_).second)
+        if(!v.metadata().is_spent_&&!(m_inputs.contains(v.metadata().outputid_)))
         {
             const auto output_=v.output();
 
             const auto  stor_unlock=output_->get_unlock_(Unlock_Condition::Storage_Deposit_Return_typ);
-            quint64 ret_amount=0;
+            quint64 retAmount=0;
+            std::shared_ptr<const Output> retOut=nullptr;
             if(stor_unlock)
             {
                 const auto sdruc=std::static_pointer_cast<const Storage_Deposit_Return_Unlock_Condition>(stor_unlock);
-                ret_amount=sdruc->return_amount();
+                retAmount=sdruc->return_amount();
                 const auto ret_address=sdruc->address();
                 const auto retUnlcon = Unlock_Condition::Address(ret_address);
-                const auto retOut = Output::Basic(ret_amount,{retUnlcon});
-                ret_outputs.push_back(retOut);
+                retOut = Output::Basic(retAmount,{retUnlcon});
             }
             const auto cday=QDateTime::currentDateTime().toSecsSinceEpoch();
             const auto time_lock=output_->get_unlock_(Unlock_Condition::Timelock_typ);
@@ -110,9 +228,9 @@ void AddressBundle::consume_outputs(std::vector<Node_output> &outs_, const quint
                 const auto unix_time=time_lock_cond->unix_time();
                 if(cday<unix_time)
                 {
-                    to_unlock.push_back(unix_time);
+                    monitorToUnlock(v.metadata().outputid_,unix_time);
                     outs_.pop_back();
-                    if(stor_unlock)ret_outputs.pop_back();
+                    retOut=nullptr;
                     continue;
                 }
             }
@@ -124,16 +242,14 @@ void AddressBundle::consume_outputs(std::vector<Node_output> &outs_, const quint
                 const auto ret_address=expiration_cond->address();
 
                 const auto  addr_unlock=std::static_pointer_cast<const Address_Unlock_Condition>(output_->get_unlock_(Unlock_Condition::Address_typ));
-                if(ret_address->addr()==get_address()->addr()&&addr_unlock->address()->addr()!=get_address()->addr())
+                if(ret_address->addr()==getAddress()->addr()&&addr_unlock->address()->addr()!=getAddress()->addr())
                 {
-                    if(stor_unlock)
-                    {
-                        ret_outputs.pop_back();
-                        ret_amount=0;
-                    }
+                    retOut=nullptr;
+                    retAmount=0;
                     if(cday<=unix_time)
                     {
                         outs_.pop_back();
+                        monitorFromExpire(v.metadata().outputid_,unix_time);
                         continue;
                     }
                 }
@@ -141,43 +257,81 @@ void AddressBundle::consume_outputs(std::vector<Node_output> &outs_, const quint
                 {
                     if(cday>unix_time)
                     {
-                        if(stor_unlock)ret_outputs.pop_back();
+                        retOut=nullptr;
                         outs_.pop_back();
                         continue;
                     }
-                    to_expire.push_back(unix_time);
+                    monitorToExpire(v.metadata().outputid_,unix_time);
                 }
-
             }
-
-
-            for(const auto& v:output_->native_tokens_)
-            {
-                native_tokens[v->token_id()]+=v->amount();
-            }
-
-            inputs.push_back(Input::UTXO(v.metadata().transaction_id_,
-                                         v.metadata().output_index_));
-
+            InBox inBox;
+            inBox.input=Input::UTXO(v.metadata().transaction_id_,v.metadata().output_index_);
 
             qblocks::c_array prevOutputSer;
             prevOutputSer.from_object<Output>(*v.output());
-            auto Input_hash=QCryptographicHash::hash(prevOutputSer, QCryptographicHash::Blake2b_256);
-            Inputs_hash+=Input_hash;
+            inBox.inputHash=QCryptographicHash::hash(prevOutputSer, QCryptographicHash::Blake2b_256);
+
             if(output_->type()!=Output::Basic_typ)
             {
                 if(output_->type()!=Output::Foundry_typ&&output_->get_id()==c_array(32,0))
                 {
                     output_->set_id(v.metadata().outputid_);
                 }
-                output_->consume();
-                if(output_->type()==Output::Foundry_typ)foundry_outputs.push_back(output_);
-                if(output_->type()==Output::Alias_typ)alias_outputs.push_back(output_);
-                if(output_->type()==Output::NFT_typ)nft_outputs.push_back(output_);
             }
-            amount+=output_->amount_-ret_amount;
+            inBox.output=output_;
+            inBox.amount+=output_->amount_-retAmount;
+            monitorToSpend(v.metadata().outputid_);
+            addInput(v.metadata().outputid_,inBox);
+
         }
         outs_.pop_back();
     }
 }
+#if defined(USE_QML)
+Qml64::Qml64(const quint64 value,QObject *parent):QObject(parent),m_value(value)
+{
+    connect(NodeConnection::instance(),&NodeConnection::stateChanged,this,&Qml64::intToJSON);
+    intToJSON();
+}
+void Qml64::setValue(const quint64 value)
+{
+    if(value!=m_value)
+    {
+        m_value=value;
+        intToJSON();
+    }
+}
+void Qml64::intToJSON()
+{
+    if(NodeConnection::instance()->state())
+    {
+        auto info=NodeConnection::instance()->rest()->get_api_core_v2_info();
+        connect(info,&Node_info::finished,this,[=]( ){
+            QJsonObject shortValue;
+            shortValue.insert("unit",info->unit);
+            shortValue.insert("value",QString::number(m_value*1.0/std::pow(10,info->decimals),'g', 5));
+
+            QJsonObject largeValue;
+            largeValue.insert("unit",info->subunit);
+            largeValue.insert("value",QString::number(m_value));
+            QJsonObject var;
+            var.insert("shortValue",shortValue);
+            var.insert("largeValue",largeValue);
+
+            if(m_value>std::pow(10,info->decimals*0.8))
+            {
+                var.insert("default",0);
+            }
+            else
+            {
+                var.insert("default",1);
+            }
+            m_json=var;
+            emit jsonChanged();
+            info->deleteLater();
+        });
+    }
+}
+#endif
+
 }
